13a25b99f89f51987ceca75772144f9706a7d34c
ramawaliyya321 <ramawaliyya@gmail.com>
2023-11-17 08:27:59 +0700

    Coba

---
+++ MODUL 3 RAMA/connect.php
@@ -0,0 +1,19 @@
+<!-- File ini berisi koneksi dengan database yang telah di import ke phpMyAdmin kalian -->
+
+
+<?php
+// Buatlah variable untuk connect ke database yang telah di import ke phpMyAdmin
+$host = "localhost:3308";
+$username = "root";
+$pw = "";
+$database = "modul3_wad";
+
+$connect = mysqli_connect($host, $username, $pw, $database);
+// 
+  
+// Buatlah perkondisian jika tidak bisa terkoneksi ke database maka akan mengeluarkan errornya
+if ($connect->connect_error){
+    echo "Koneksi Gagal!";
+}
+// 
+?>
